b757cb86c272207a182f8ea2a7c27f04241d3f7f
Benedetto Lo Giudice <benedetto.logiudice@anchor.com.au>
2016-09-21 11:23:27 +0100

    Changes necessary to enable JMX

--- puppetserver-standalone/docker-entrypoint.sh
+++ puppetserver-standalone/docker-entrypoint.sh
@@ -6,4 +6,8 @@ if test -n "${PUPPETDB_SERVER_URLS}" ; then
   sed -i "s@^server_urls.*@server_urls = ${PUPPETDB_SERVER_URLS}@" /etc/puppetlabs/puppet/puppetdb.conf
 fi
 
+echo "******************************************"
+echo "about to execute the puppet server with $@"
+echo "******************************************"
+
 exec /opt/puppetlabs/bin/puppetserver "$@"
